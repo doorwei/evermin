e71cc5d064b9b6cca84778e3d6992d5cc68e5176
sxcw <yuan329419@gmail.com>
2018-06-27 06:46:03 -0700

    Setup firebase by running firebase init

--- src/index.js
+++ src/index.js
@@ -7,6 +7,30 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 
+// Firebase App is always required and must be first
+var firebase = require("firebase/app");
+
+// Add additional services that you want to use
+require("firebase/auth");
+require("firebase/database");
+require("firebase/firestore");
+require("firebase/messaging");
+require("firebase/functions");
+
+// Comment out (or don't require) services that you don't want to use
+// require("firebase/storage");
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBQj90ic_PzH6QrQmbgE11hnHInf73HJyA",
+  authDomain: "evermin-firebase.firebaseapp.com",
+  databaseURL: "https://evermin-firebase.firebaseio.com",
+  projectId: "evermin-firebase",
+  storageBucket: "evermin-firebase.appspot.com",
+  messagingSenderId: "132834061455"
+};
+firebase.initializeApp(config);
+
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
